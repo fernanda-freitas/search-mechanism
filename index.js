fc0392444155ab718928591dcd099667f1352996
Fernanda Freitas Martinez <idea.fernanda@gmail.com>
2022-06-20 17:40:54 +0100

    search movies mechanism

---
+++ index.js
@@ -0,0 +1,25 @@
+"use strict";
+const form = document.querySelector("#search-form");
+const searchInput = document.getElementById("search-input");
+const submitButton = document.querySelector(".btn-primary");
+const results = document.getElementById("results");
+
+const searchMovies = (query) => {
+  fetch(`http://www.omdbapi.com/?s=${query}&apikey=48727053`)
+  .then(response => response.json())
+  .then((data) => {
+    data.Search.forEach((result) => {
+      const movieElement = `<li class="list-item">
+        <img class="movie-poster" src="${result.Poster}" alt="movie poster">
+        <p class="movie-title">${result.Title}</p>`;
+      results.insertAdjacentHTML("beforeend", movieElement);
+    });
+  })
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  results.innerHTML = "";
+  console.log(searchInput.value);
+  searchMovies(searchInput.value);
+})
